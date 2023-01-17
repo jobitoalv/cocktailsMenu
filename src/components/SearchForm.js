6f0d4ab7ed26e5d6f8d07ea3c0b5de760355b8ff
Job Alvarez <jobitoalv@gmail.com>
2023-01-17 15:41:20 +1000

    fix search form bug

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,31 +1,33 @@
 import React from 'react'
 import { useGlobalContext } from '../context'
-
-const SearchForm = () => {
-  const {setSearchTerm} = useGlobalContext()
+export default function SearchForm() {
+  const { setSearchTerm } = useGlobalContext()
   const searchValue = React.useRef('')
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     searchValue.current.focus()
   }, [])
 
-  const searchCocktail = () =>{
+  function searchCocktail() {
     setSearchTerm(searchValue.current.value)
   }
-  const handleSubmit = () =>{
+  function handleSubmit(e) {
     e.preventDefault()
   }
-  
   return (
     <section className='section search'>
-      <form className='search-form'onSubmit={handleSubmit}>
+      <form className='search-form' onSubmit={handleSubmit}>
         <div className='form-control'>
-          <label htmlFor='name'> search your favorite cocktail</label>
-          <input type='text' id='name' ref={searchValue} onChange={searchCocktail}/>
+          <label htmlFor='name'>search your favorite cocktail</label>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
         </div>
       </form>
     </section>
   )
 }
-
-export default SearchForm
