9190c853c7f1ac9b91fcb293fd0d95ef0fc648da
Job Alvarez <jobitoalv@gmail.com>
2023-01-17 15:17:37 +1000

    getting the right cocktails details when search

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -5,15 +5,17 @@ const SearchForm = () => {
   const {setSearchTerm} = useGlobalContext()
   const searchValue = React.useRef('')
 
+  const searchCocktail = () =>{
+    setSearchTerm(searchValue.current.value)
+  }
   return (
     <section className='section search'>
       <form className='search-form'>
         <div className='form-control'>
           <label htmlFor='name'> search your favorite cocktail</label>
-          <input type='text' id='name' ref={searchValue} />
+          <input type='text' id='name' ref={searchValue} onChange={searchCocktail}/>
         </div>
       </form>
-      <h2>search form component</h2>
     </section>
   )
 }
